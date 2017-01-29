58147aa61e7ab926f03b43f3b083542f4bc16eea
razonrus <iruslansafin@gmail.com>
2017-01-29 18:52:33 +0500

    add blank search

---
+++ SearchEngine/ISearch.cs
@@ -0,0 +1,15 @@
+﻿namespace SearchEngine
+{
+    public interface ISearch
+    {
+        User[] Search(string query);
+    }
+
+    public class DefaultSearch: ISearch
+    {
+        public User[] Search(string query)
+        {
+            return new User[0];
+        }
+    }
+}
---
+++ SearchEngine/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SearchEngine
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
