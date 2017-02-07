ba86c2e5185aa7ce98bdab2f4d92ec5f20ef68d9
razonrus <iruslansafin@gmail.com>
2017-02-08 00:18:19 +0500

    add tool for check results

---
+++ Tools/Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Tools
+{
+    public class Tests
+    {
+        private const string BasePath = @"";
+
+        [Test]
+        public void Test()
+        {
+            var lines = File.ReadAllLines(BasePath + "orig.txt");
+
+            var emails = lines.Select(x => x.Split(',').First().ToLower()).ToList();
+           
+            var file = File.ReadAllText(BasePath + "orig.txt").ToLower();
+
+            var foundEmailsCount = emails.Count(x =>
+                //new Regex(@"(\W|^)(" + Regex.Escape(x) + @")(\W|$)", RegexOptions.Multiline).IsMatch(file)
+                file.Contains(x)
+                );
+
+            Console.WriteLine(foundEmailsCount);
+            Console.WriteLine(foundEmailsCount / (decimal) emails.Count());
+        }
+    }
+}
